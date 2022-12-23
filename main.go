c776e6049e23ceb9d5e29dca5cecb65dc1d80e73
LidiaMil <lidamilos@rambler.ru>
2022-12-23 03:20:21 +0300

    add http

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"net/http"
+		)
+
+func home_page(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello YOU!!!")
+}
 
 func main()  {
 	fmt.Println("hello you")
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
 }
