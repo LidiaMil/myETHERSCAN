4eb32254ec3eb4041eb961ef5791cc97e7677e66
LidiaMil <lidamilos@rambler.ru>
2022-12-23 03:26:16 +0300

    add struct

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import ("fmt"
 		"net/http"
 		)
 
+type User struct{
+	name string
+	age uint16
+	money int16
+	avg_grades, happiness float32
+}
+
 func home_page(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello YOU!!!")
 }
@@ -17,8 +24,8 @@ func handleRequest()  {
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/contacts/", contacts_page)
 	http.ListenAndServe(":8080", nil)
-
 }
+
 func main()  {
 	handleRequest()
 }
