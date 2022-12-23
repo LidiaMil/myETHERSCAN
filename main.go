87ec5b1fad0135acf5b8427519a07e955b5e7537
LidiaMil <lidamilos@rambler.ru>
2022-12-23 03:44:09 +0300

    home page

--- main.go
+++ main.go
@@ -6,19 +6,19 @@ import ("fmt"
 		)
 
 type User struct{
-	name string
-	age uint16
-	money int16
-	avg_grades, happiness float32
+	Name string
+	Age uint16
+	Money int16
+	Avg_grades, happiness float32
 }
 
 func (u User) getAllInfo() string{
-	return fmt.Sprintf("User name is: %s. He is %d. He has money = %d", u.name, u.age, u.money)
+	return fmt.Sprintf("User name is: %s. He is %d. He has money = %d", u.Name, u.Age, u.Money)
 
 }
 
 func (u *User) setNewName(newName string){
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request){
