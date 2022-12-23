a9e8c08c9010a6b1f1961171bda5b452934f0788
LidiaMil <lidamilos@rambler.ru>
2022-12-23 03:23:10 +0300

    add page

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ func home_page(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello YOU!!!")
 }
 
-func main()  {
+func contacts_page(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "contacts page !!!")
+}
+
+func handleRequest()  {
 	fmt.Println("hello you")
 	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
 	http.ListenAndServe(":8080", nil)
+
+}
+func main()  {
+	handleRequest()
 }
