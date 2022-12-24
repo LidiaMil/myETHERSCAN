d1d9266102f3631528d7b28f772278d96c6242ab
LidiaMil <lidamilos@rambler.ru>
2022-12-24 03:31:44 +0300

    connect mysql

--- main.go
+++ main.go
@@ -8,5 +8,13 @@ import ("fmt"
 
 func main()  {
 	fmt.Println("работа с mysql")
-	
+	db, err := sql.Open("mysql","root:root@tcp(127.0.0.1:8889)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	fmt.Println("подключено к mysql")
+
 }
